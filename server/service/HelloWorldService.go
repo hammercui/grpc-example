aa585538e1ef92984af16e26a1d185f4ecce3d8d
hammercui <hammercui@163.com>
2021-10-15 17:46:38 +0800

    fix: 修復啓動bug

--- server/service/HelloWorldService.go
+++ server/service/HelloWorldService.go
@@ -2,24 +2,12 @@ package service
 
 import (
 	"context"
+	"fmt"
 	rpc_package "grpc-example/api"
+	"io"
 )
 
 type HelloWordlService struct {
-
-
-}
-
-func (p *HelloWordlService) mustEmbedUnimplementedHelloWorldServiceServer() {
-	panic("implement me")
-}
-
-func (p *HelloWordlService) SayHello(ctx context.Context, request *rpc_package.HelloRequest) (*rpc_package.HelloReply, error) {
-	panic("implement me")
-}
-
-func (p *HelloWordlService) BidirectionalStream(server rpc_package.HelloWorldService_BidirectionalStreamServer) error {
-	panic("implement me")
 }
 
 func NewHelloWorldService() *HelloWordlService {
@@ -27,36 +15,39 @@ func NewHelloWorldService() *HelloWordlService {
 	}
 }
 
-//func (p *HelloWordlService) SayHello(ctx context.Context, in *rpc_package.HelloRequest) (*rpc_package.HelloReply, error){
-//	fmt.Println("receive req:",in)
-//	return &rpc_package.HelloReply{Message:"success"},nil
-//}
-//
-////双向流通信
-//func (p *HelloWordlService) BidirectionalStream(stream rpc_package.HelloWorldService_BidirectionalStreamServer) error {
-//	for{
-//		n := 0
-//		//server send
-//		err := stream.Send(&rpc_package.SteamResponse{
-//			OptCode:              int32(n),
-//			RespCode:             0,
-//			Output:               "server response",
-//		})
-//		if err != nil{
-//			fmt.Println("server send error:",err)
-//			return err
-//		}
-//		//server rec
-//		r,err := stream.Recv()
-//		if err == io.EOF{
-//			fmt.Println("cliet disconnect,:",err)
-//			return nil
-//		}
-//		if err != nil{
-//			fmt.Println("client err:",err)
-//			return  err
-//		}
-//		n++
-//		fmt.Println("server rec info",r)
-//	}
-//}
+func (p *HelloWordlService) mustEmbedUnimplementedHelloWorldServiceServer() {
+	panic("implement me")
+}
+
+func (p *HelloWordlService) SayHello(ctx context.Context, request *rpc_package.HelloRequest) (*rpc_package.HelloReply, error) {
+	fmt.Println("receive req:",request)
+	return &rpc_package.HelloReply{Message:"success"},nil
+}
+
+func (p *HelloWordlService) BidirectionalStream(stream rpc_package.HelloWorldService_BidirectionalStreamServer) error {
+		for{
+			n := 0
+			//server send
+			err := stream.Send(&rpc_package.SteamResponse{
+				OptCode:              int32(n),
+				RespCode:             0,
+				Output:               "server response",
+			})
+			if err != nil{
+				fmt.Println("server send error:",err)
+				return err
+			}
+			//server rec
+			r,err := stream.Recv()
+			if err == io.EOF{
+				fmt.Println("cliet disconnect,:",err)
+				return nil
+			}
+			if err != nil{
+				fmt.Println("client err:",err)
+				return  err
+			}
+			n++
+			fmt.Println("server rec info",r)
+		}
+}
